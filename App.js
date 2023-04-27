5a416d12b905951b6b214742f9bab4fa72720602
Alwallo <alvgonzalezb@gmail.com>
2023-04-27 12:24:22 -0600

    Agregar tareas a la lista

---
+++ App.js
@@ -0,0 +1,22 @@
+const inputTarea = document.getElementById('inputForm');
+const btnAgregar = document.getElementById('btnCreate');
+
+btnAgregar.addEventListener('click', (event) => {
+  event.preventDefault();
+  const inputTexto = inputTarea.value;
+
+  const tareas = document.getElementById('cardsList');
+  const newLista = document.createElement('li');
+  newLista.classList.add('card');
+
+  newLista.innerHTML = `
+    <div>
+      <i class="fa fa-check-square icon"></i>
+      <span class="task">${inputTexto}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>
+  `;
+
+  tareas.appendChild(newLista);
+  inputTarea.value = '';
+});
